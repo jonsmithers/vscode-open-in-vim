c3316092818342e67dbb0f75882a108216c06de9
Jon Smithers <mail@jonsmithers.link>
2017-12-25 19:14:49 -0500

    Prepare for multiple open methods

--- extension.js
+++ extension.js
@@ -33,39 +33,52 @@ function deactivate() {
 exports.deactivate = deactivate;
 
 function openInVim() {
-    let activeTextEditor = vscode.window.activeTextEditor;
-    if (!activeTextEditor) {
-        vscode.window.showErrorMessage('No active editor.');
+    let {openMethod} = vscode.workspace.getConfiguration()["open-in-vim"]
+    let actualOpenMethod = openMethods[openMethod];
+    if (!actualOpenMethod) {
+        let availableMethods = Object.keys(openMethods).map(name => `"${name}"`).join(", ");
+        vscode.window.showErrorMessage(`Check your settings. Method "${openMethod}" is not supported. Currently, you can use ${availableMethods}.`);
         return;
     }
-    if (activeTextEditor.document.isUntitled) {
-        vscode.window.showErrorMessage('Please save the file first.');
-        return;
-    }
-    if (activeTextEditor.document.isDirty) {
-        activeTextEditor.document.save();
-    }
-    const position = activeTextEditor.selection.active;
-    let fileName = activeTextEditor.document.fileName;
-    let line = position.line+1
-    let column = position.character+1
-    let workspace = vscode.workspace.getWorkspaceFolder(activeTextEditor.document.uri);
-    if (!workspace) {
-        // user opened a file outside of any workspace
-        workspace = vscode.workspace.workspaceFolders[0]
-        vscode.window.showWarningMessage(`Defaulting workspace to ${workspace.name}`);
-    }
-    let workspacePath = workspace.uri.path;
-    // let extensionPath = vscode.extensions.all.find(e => e.id.includes("open-in-vim")).extensionPath;
-    let osascriptcode = `
-        tell application "iTerm"
-          set myNewWin to create window with default profile
-          tell current session of myNewWin
-            write text "cd '${workspacePath}'"
-            write text "vim '${fileName}' '+call cursor(${line}, ${column})'; exit"
-          end tell
-        end tell
-    `;
-    let result = require('child_process').spawnSync("/usr/bin/osascript", {encoding: "utf8", input: osascriptcode})
-    // check for errors here?
+    actualOpenMethod();
 }
+
+const openMethods = {
+    "osx.iterm": function() {
+        let activeTextEditor = vscode.window.activeTextEditor;
+        if (!activeTextEditor) {
+            vscode.window.showErrorMessage('No active editor.');
+            return;
+        }
+        if (activeTextEditor.document.isUntitled) {
+            vscode.window.showErrorMessage('Please save the file first.');
+            return;
+        }
+        if (activeTextEditor.document.isDirty) {
+            activeTextEditor.document.save();
+        }
+        const position = activeTextEditor.selection.active;
+        let fileName = activeTextEditor.document.fileName;
+        let line = position.line+1
+        let column = position.character+1
+        let workspace = vscode.workspace.getWorkspaceFolder(activeTextEditor.document.uri);
+        if (!workspace) {
+            // user opened a file outside of any workspace
+            workspace = vscode.workspace.workspaceFolders[0]
+            vscode.window.showWarningMessage(`Defaulting workspace to ${workspace.name}`);
+        }
+        let workspacePath = workspace.uri.path;
+        // let extensionPath = vscode.extensions.all.find(e => e.id.includes("open-in-vim")).extensionPath;
+        let osascriptcode = `
+            tell application "iTerm"
+              set myNewWin to create window with default profile
+              tell current session of myNewWin
+                write text "cd '${workspacePath}'"
+                write text "vim '${fileName}' '+call cursor(${line}, ${column})'; exit"
+              end tell
+            end tell
+        `;
+        let result = require('child_process').spawnSync("/usr/bin/osascript", {encoding: "utf8", input: osascriptcode})
+        // check for errors here?
+    }
+};
